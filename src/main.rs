f0fc87660868afb066c80cf0c728240a74f052ca
James Miller <jamesmiller01@gmail.com>
2022-03-06 20:34:48 -0500

    Add logging support with tracing lib
    - init logging
    - logging uses RUST_LOG
    - add info log

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
-use lambda_runtime::{service_fn, LambdaEvent, Error};
+use lambda_runtime::{service_fn, Error, LambdaEvent};
 use serde_json::{json, Value};
+use tracing::info;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    init_lambda_tracing();
     let func = service_fn(func);
     lambda_runtime::run(func).await?;
     Ok(())
 }
 
+fn init_lambda_tracing() {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        // this needs to be set to false, otherwise ANSI color codes will
+        // show up in a confusing manner in CloudWatch logs.
+        .with_ansi(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+}
+
 async fn func(event: LambdaEvent<Value>) -> Result<Value, Error> {
     let (event, _context) = event.into_parts();
     let first_name = event["firstName"].as_str().unwrap_or("world");
-
+    info!("going to say hello to {}", first_name);
     Ok(json!({ "message": format!("Hello, {}!", first_name) }))
 }
